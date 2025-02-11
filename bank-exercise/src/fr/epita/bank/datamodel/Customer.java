b4009d850b52950e4d7de3cd43519d792f889269
Présentation <tbr@tbr.com>
2025-02-11 08:55:38 +0100

    introduction of scanner

--- bank-exercise/src/fr/epita/bank/datamodel/Customer.java
+++ bank-exercise/src/fr/epita/bank/datamodel/Customer.java
@@ -4,8 +4,18 @@ public class Customer {
     String name;
     String address;
 
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
     public Customer(String newName, String newAddress) {
         this.name = newName;
         this.address = newAddress;
     }
+
+
 }
--- bank-exercise/src/fr/epita/bank/launcher/Main.java
+++ bank-exercise/src/fr/epita/bank/launcher/Main.java
@@ -4,19 +4,29 @@ import fr.epita.bank.datamodel.Customer;
 import fr.epita.bank.datamodel.SavingsAccount;
 import fr.epita.bank.service.AccountService;
 
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        // 1.create a customer named quentin
+        // 1.create a customer named customer
         // 2. assign to this customer a savings account initialized with 500€
         // 3. calculate the yearly gain linked to the interest rate
         // 4. add the gain to the savings balance
         // 5. the customer wants to withdraw 300€ then 500€, if the balance is
         // insufficient, then it should print a warning
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to this bank application");
 
-        Customer quentin = new Customer("quentin", "paris");
-        SavingsAccount savingsAccount = new SavingsAccount(500.0, quentin);
+        System.out.println("Account creation, enter the customer name");
+        String name = scanner.nextLine();
+        System.out.println(" enter the customer address");
+        String address = scanner.nextLine();
+        Customer customer = new Customer(name,address);
+        System.out.println("you have created this user: " + customer);
+
+        SavingsAccount savingsAccount = new SavingsAccount(500.0, customer);
         AccountService.creditInterest(savingsAccount);
 
         //5.
