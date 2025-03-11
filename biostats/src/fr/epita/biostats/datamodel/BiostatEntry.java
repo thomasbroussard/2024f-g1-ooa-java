ff1f9740e271bd32241d27328eea2686cbc1aad5
Présentation <tbr@tbr.com>
2025-03-11 09:51:42 +0100

    biostats 3rd checkpoint

--- biostats/src/fr/epita/biostats/datamodel/BiostatEntry.java
+++ biostats/src/fr/epita/biostats/datamodel/BiostatEntry.java
@@ -1,11 +1,57 @@
 package fr.epita.biostats.datamodel;
 
 public class BiostatEntry {
-   // "Name",     "Sex", "Age", "Height (in)", "Weight (lbs)"
     private String name;
     private String gender;
     private Integer age;
     private Integer height;
     private Integer weight;
 
+    public BiostatEntry(String name, String gender, Integer age, Integer height, Integer weight) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
 }
--- biostats/test/fr/epita/biostats/tests/SandboxTest.java
+++ biostats/test/fr/epita/biostats/tests/SandboxTest.java
@@ -1,31 +1,61 @@
 package fr.epita.biostats.tests;
 
+import fr.epita.biostats.datamodel.BiostatEntry;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SandboxTest {
 
     public static void main(String[] args) throws IOException {
 
-        List<String> lines = Files.readAllLines(Path.of("biostats/biostats.csv"));
-        //for each line in lines we do the following
-        String line = "\"Alex\",       \"M\",   41,       74,      170";
-        String[] parts = line.split(",");
-        String cleanName = parts[0].replace("\"", "").strip();
-        String cleanGender = parts[1].replace("\"", "").strip();
-        System.out.println(cleanName);
-        Integer cleanAge = Integer.parseInt(parts[2].strip());
-        Integer cleanHeight = Integer.parseInt(parts[3].strip());
-        Integer cleanWeight = Integer.parseInt(parts[4].strip());
-        System.out.println(cleanAge);
+        List<BiostatEntry> entries = importBiostatEntriesFromCSV();
 
-        //we assign all the variables to an instance of BiostatEntry
-        //we add the biostat entry in the list
+
         //we want to write all the data into a new file
         //we iterate over the biostat entry list
-        //for each element, we write the csv output corresponding to the entry
+        exportBiostatEntries(entries);
 
+
+    }
+
+    private static void exportBiostatEntries(List<BiostatEntry> entries) throws IOException {
+        String csvOutput = "";
+        for (BiostatEntry currentEntry : entries){
+            //TODO format the entry to CSV
+            csvOutput = csvOutput +
+                    currentEntry.getName() +"," +
+                    currentEntry.getGender() +"," +
+                    currentEntry.getAge() +"," +
+                    currentEntry.getHeight() +"," +
+                    currentEntry.getWeight()
+                    + System.lineSeparator();
+
+        }
+        Files.writeString(Path.of("biostats/output.csv"), csvOutput);
+    }
+
+    private static List<BiostatEntry> importBiostatEntriesFromCSV() throws IOException {
+        List<BiostatEntry> entries = new ArrayList<>();
+        List<String> lines = Files.readAllLines(Path.of("biostats/biostats.csv"));
+        //for each line in lines we do the following
+        for (String line : lines) {
+
+            String[] parts = line.split(",");
+            String cleanName = parts[0].replace("\"", "").strip();
+            String cleanGender = parts[1].replace("\"", "").strip();
+            System.out.println(cleanName);
+            Integer cleanAge = Integer.parseInt(parts[2].strip());
+            Integer cleanHeight = Integer.parseInt(parts[3].strip());
+            Integer cleanWeight = Integer.parseInt(parts[4].strip());
+            //we assign all the variables to an instance of BiostatEntry
+            BiostatEntry entry = new BiostatEntry(cleanName, cleanGender, cleanAge,cleanHeight, cleanWeight);//TODO assign with the variables
+            //we add the biostat entry in the list
+            entries.add(entry);
+        }
+        return entries;
     }
 }
